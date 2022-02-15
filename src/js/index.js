cb3df0a02ab82a0124fc69049ca04a386c40d20b
Head4oT <max.pivovar@gmail.com>
2022-02-15 19:46:48 +0200

    Header + menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
 window.$ = window.jQuery = require('jquery');
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+Swiper.use([ Autoplay, Navigation, Pagination]);
+
+$(document).ready(function() {
+    $('.section-title').on('click', function() {
+        $('.header__menu').toggleClass('active');
+        $('.section-title').toggleClass('active');
+        $('.overlay').toggleClass('active');
+        $('.header__menu-tabs').fadeToggle();
+        $('.header__menu-tabs-content').fadeToggle();
+    })
+
+    //Portfolio swiper
+    const portfolio__swiper = new Swiper('.portfolio__swiper', {
+        loop: true,
+        // autoplay: {
+        //     delay: 5000,
+        // },
+        slidesPerView: 1,
+        spaceBetween: 30,
+        autoHeight: 'true',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
+    //Tabs
+    $(".tabs__item").eq(0).fadeIn();
+    $('.tabs__button').on('click', function () {
+        $(".tabs__button").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tabs__item").hide().eq($(this).index()).fadeIn();
+    }).eq(0).addClass("active");
+    $(".tabs__item").eq(0).fadeIn();
+})
+
+
